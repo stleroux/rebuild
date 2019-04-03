39e24d1202631e20e4f8b7a680cd188628c3e7d6
Stephane Leroux <stleroux@hotmail.ca>
2019-04-03 15:28:03 -0400

    Initial commit

--- modules/Recipes/Resources/views/frontend/_index.blade.php
+++ modules/Recipes/Resources/views/frontend/_index.blade.php
@@ -38,75 +38,9 @@
 		</div>
 
 		@if($recipes->count() > 0)
-
-			<div class="card-body pt-1">
-				<div class="mt-0 mb-2">
-					@include('recipes::frontend.alphabet', ['model'=>'recipe'])
-				</div>
-				
-					<div class="row">
-						@foreach($recipes as $recipe)
-							<div class="card col-sm-6 col-md-4 col-lg-3 col-xl-2 p-0" onMouseOver="this.style.backgroundColor='#C0C0C0'" onMouseOut="this.style.backgroundColor=''">
-								@if($recipe->image)
-									<a href="{{ route('recipes.show', $recipe->id) }}" class="" style="text-decoration: none">
-										<img class="card-img-top" src="\_recipes\{{ $recipe->image }}" height="100px" width="100%">
-									</a>
-								@else
-									<a href="{{ route('recipes.show', $recipe->id) }}" class="" style="text-decoration: none">
-										<img class="card-img-top" src="\_recipes\image_not_available.jpg" height="100px" width="100%">
-									</a>
-								@endif
-
-								<div class="card-body pt-1 pb-0">
-									<a href="{{ route('recipes.show', $recipe->id) }}" class="" style="text-decoration: none">
-										<h6 class="card-title text-center pb-2 m-0">
-											{{ ucwords($recipe->title) }}
-										</h6>
-									</a>
-								</div>
-
-								<div class="card-text p-0 m-0 text-center">
-									<div class="align-self-end">
-										<p>
-											<span class="badge badge-light text-dark" title="Times Viewed">{{ $recipe->views }} Views</span>
-											<span class="badge badge-light text-dark" title="Comments">{{ $recipe->comments->count() }} Comments</span>
-											<br />
-											<span class="badge badge-light text-dark" title="Times Favorited">{{ \Modules\Recipes\Entities\Recipe::find($recipe->id)->favoritesCount }} Favorited</span>
-										</p>
-									</div>	
-								</div>
-
-								@auth
-									<div class="card-text pb-1 m-0">
-										<div class="align-self-end text-center">
-											@if(!$recipe->isFavorited())
-												@include('common.buttons.favoriteAdd', ['model'=>'recipe', 'id'=>$recipe->id, 'type'=>''])
-											@else
-												@include('common.buttons.favoriteRemove', ['model'=>'recipe', 'id'=>$recipe->id, 'type'=>''])
-											@endif
-										</div>
-									</div>
-								@endauth
-
-								<div class="card-footer px-1 py-0 text-center">
-									<small class="">
-										By
-										@if($recipe->user->profile->first_name && $recipe->user->profile->last_name)
-											{{ ucwords($recipe->user->profile->first_name) }} {{ ucwords($recipe->user->profile->last_name) }}
-										@else
-											{{ $recipe->user->username }}
-										@endif
-									</small>
-								</div>
-							</div>
-						@endforeach
-					</div>
-				
-			</div>
-
-
-{{-- 			<div class="card-body card_body p-2 bg-transparent">
+			<div class="card-body card_body p-2 bg-transparent">
 				@include('recipes::frontend.alphabet', ['model'=>'recipe'])
+
 				@foreach($recipes->chunk(6) as $chunk)
 					<div class="card-deck mb-0 px-2 bg-transparent">
 						@foreach($chunk as $recipe)
@@ -165,7 +99,7 @@
 						@endforeach
 					</div>
 				@endforeach
-			</div> --}}
+			</div>
 
 			<div class="card-footer px-1 py-1">
 				{{ $recipes->links() }}
