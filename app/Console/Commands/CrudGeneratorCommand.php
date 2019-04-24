5fcbbe77da90136758790b2245a8bea312d13f14
Stephane Leroux <stleroux@hotmail.ca>
2019-04-24 15:32:37 -0400

    Initial commit

--- app/Console/Commands/CrudGeneratorCommand.php
+++ app/Console/Commands/CrudGeneratorCommand.php
@@ -8,51 +8,55 @@ use File;
 
 class CrudGeneratorCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'crud:generator {name : Class (singular), e.g.: User}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+
     protected $description = 'Create CRUD operations';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
+
     public function handle()
     {
         // Get the name of the argument
         $name = $this->argument('name');
 
         // Create the files
+        $this->info('Creating controller');
         $this->controller($name);
+        
+        $this->info('Creating model');
         $this->model($name);
+
+        $this->info('Creating request');
         $this->request($name);
 
+        $this->info('Creating Create view');
+        $this->createView($name);
+
+        $this->info('Creating Delete view');
+        $this->deleteView($name);
+
+        $this->info('Creating Edit view');
+        $this->editView($name);
+
+        $this->info('Creating Index view');
+        $this->indexView($name);
+
+        $this->info('Creating Show view');
+        $this->showView($name);
+
         // Add resource controller to routes files
-        File::append(base_path('routes/web.php'), 'Route::resource(\'' . strtolower(Str::plural($name)) . "', '" . (Str::plural($name)) . "Controller');");
+        $this->info('Adding resource route');
+        File::append(base_path('routes/web.php'), 'Route::resource(\'' . strtolower(Str::plural($name)) . "', '" . (Str::plural($name)) . "Controller');"). "<br />";
 
         // Create a migration file
-        // Artisan::call('make:migration create_' . strtolower(Str::plural($name)) . '_table --create=' . strtolower(Str::plural($name)));
-        // Artisan::call('migrate', array('--path' => 'database/migrations', '--force' => true));
+        $this->info('Creating migration');
         \Artisan::call('make:migration', 
             [
                 'name'=>'create_' . strtolower(Str::plural($name)) . '_table',
@@ -60,13 +64,17 @@ class CrudGeneratorCommand extends Command
             ]
         );
 
+        $this->info('');
+        $this->info('Done. Don\' forget to update the migration file and then run "php artisan migrate"');
+
     }
+
 
     protected function getStub($type)
     {
-        // return file_get_contents(resource_path("\stubs\'" . $type . ".stub"));
         return file_get_contents(resource_path("stubs/$type.stub"));
     }
+
 
     protected function model($name)
     {
@@ -78,6 +86,7 @@ class CrudGeneratorCommand extends Command
 
         file_put_contents(app_path("/{$name}.php"), $template);
     }
+
 
     protected function request($name)
     {
@@ -93,6 +102,7 @@ class CrudGeneratorCommand extends Command
         file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $template);
     }
 
+
     protected function controller($name)
     {
         $template = str_replace(
@@ -103,16 +113,132 @@ class CrudGeneratorCommand extends Command
                 '{{modelNamePlural}}',
             ],
             [
-                $name, // Place
-                strtolower(Str::plural($name)), //places
-                strtolower($name), // place
-                Str::plural($name)
+                $name,                          // Place
+                strtolower(Str::plural($name)), // places
+                strtolower($name),              // place
+                Str::plural($name)              // Places
             ],
             $this->getStub('Controller')
         );
 
         file_put_contents(app_path("/Http/Controllers/".Str::plural($name)."Controller.php"), $template);
-        
     }
+
+
+    protected function createView($name)
+    {
+        $template = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}',
+                '{{modelNamePlural}}',
+            ],
+            [
+                $name,                          // Place
+                strtolower(Str::plural($name)), // places
+                strtolower($name),              // place
+                Str::plural($name)              // Places
+            ],
+            $this->getStub('views/create')
+        );
+
+        if(!file_exists($path = resource_path("/views/" . strtolower(Str::plural($name)))))
+            mkdir($path, 0777, true);
+
+        file_put_contents(resource_path("/views/".Str::plural($name)."/create.blade.php"), $template);
+    }
+
+
+    protected function deleteView($name)
+    {
+        $template = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}',
+                '{{modelNamePlural}}',
+            ],
+            [
+                $name,                          // Place
+                strtolower(Str::plural($name)), // places
+                strtolower($name),              // place
+                Str::plural($name)              // Places
+            ],
+            $this->getStub('views/delete')
+        );
+
+        file_put_contents(resource_path("/views/".Str::plural($name)."/delete.blade.php"), $template);
+    }
+
+
+    protected function editView($name)
+    {
+        $template = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}',
+                '{{modelNamePlural}}',
+            ],
+            [
+                $name,                          // Place
+                strtolower(Str::plural($name)), // places
+                strtolower($name),              // place
+                Str::plural($name)              // Places
+            ],
+            $this->getStub('views/edit')
+        );
+
+        file_put_contents(resource_path("/views/".Str::plural($name)."/edit.blade.php"), $template);
+    }
+
+
+    protected function indexView($name)
+    {
+        $template = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}',
+                '{{modelNamePlural}}',
+            ],
+            [
+                $name,                          // Place
+                strtolower(Str::plural($name)), // places
+                strtolower($name),              // place
+                Str::plural($name)              // Places
+            ],
+            $this->getStub('views/index')
+        );
+
+        // if(!file_exists($path = resource_path("/views/" . strtolower(Str::plural($name)))))
+        //     mkdir($path, 0777, true);
+
+        file_put_contents(resource_path("/views/".Str::plural($name)."/index.blade.php"), $template);
+    }
+
+
+    protected function showView($name)
+    {
+        $template = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}',
+                '{{modelNamePlural}}',
+            ],
+            [
+                $name,                          // Place
+                strtolower(Str::plural($name)), // places
+                strtolower($name),              // place
+                Str::plural($name)              // Places
+            ],
+            $this->getStub('views/show')
+        );
+
+        file_put_contents(resource_path("/views/".Str::plural($name)."/show.blade.php"), $template);
+    }
+
 
 }
---
+++ resources/views/tests/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.')
+
+@section('stylesheets')
+   {{-- {{ Html::style('css/recipes.css') }} --}}
+@endsection
+
+@section('left_column')
+   {{-- @include('blocks.adminNav') --}}
+   {{-- @include('tests.sidebar') --}}
+@endsection
+
+@section('right_column')
+@endsection
+
+@section('content')
+
+   <div class="row">
+      <div class="col">
+         <div class="card mb-2">
+            <!--CARD HEADER-->
+            <div class="card-header card_header">
+               {{-- <i class="fa fa-sitemap"></i> --}}
+               Tests
+               <span class="float-right">
+                  @include('common.buttons.help', ['bookmark'=>'tests'])
+                  @include('common.buttons.add', ['model'=>'test', 'type'=>''])
+               </span>
+            </div>
+
+            <!--CARD BODY-->
+            @if($tests->count() > 0)
+               <div class="card-body card_body pb-0">
+                  @include('common.alphabet', ['model'=>'test', 'page'=>'index'])
+                  <table id="datatable" class="table table-hover table-sm">
+                     <thead>
+                        <tr>
+                           <th>ID</th>
+                           {{-- Add more columns here --}}
+                           <th>Created</th>
+                           <th class="no-sort"></th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        @foreach ($tests as $test)
+                           <tr>
+                              <td>{{ $test->id }}</td>
+                              {{-- Add more columns here --}}
+                              <td data-order="{{ $test->created_at}}">{{ $test->created_at ? $test->created_at->format('M d, Y') : 'no data found' }}</td>
+                              <td class="text-right">
+                                 @if(checkPerm('test_edit'))
+                                    @include('common.buttons.edit', ['model'=>'test', 'id'=>$test->id])
+                                 @endif
+
+                                 @if(checkPerm('test_delete'))
+                                    @include('common.buttons.delete', ['model'=>'test', 'id'=>$test->id])
+                                 @endif
+                              </td>
+                           </tr>
+                        @endforeach
+                     </tbody>
+                  </table>
+               </div>
+            @else
+               <div class="card-body card_body">
+                  {{ setting('no_records_found') }}
+               </div>
+            @endif
+            
+         </div>
+      </div>
+   </div>
+
+@endsection
