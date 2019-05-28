86a41598fefffccfbd821fbad0fe3f1401b01ccf
Stephane Leroux <stleroux@hotmail.ca>
2019-05-28 15:27:12 -0400

    Initial commit

--- resources/views/recipes/addins/links/favorite.blade.php
+++ resources/views/recipes/addins/links/favorite.blade.php
@@ -1,13 +1,15 @@
-@if(!$recipe->isFavorited())
-   <a href="{{ route('recipes.favoriteAdd', $recipe->id) }}"
-      class="btn btn-sm btn-primary"
-      title="Add Favorite">
-      <i class="{{ Config::get('buttons.favorite') }} text-danger"></i>
-   </a>
-@else
-   <a href="{{ route('recipes.favoriteRemove', $recipe->id) }}"
-      class="btn btn-sm btn-primary"
-      title="Remove Favorite">
-      <i class="{{ Config::get('buttons.favorite') }} text-success"></i>
-   </a>
-@endif
+{{-- @if(checkPerm('recipe_favorite', $recipe)) --}}
+   @if(!$recipe->isFavorited())
+      <a href="{{ route('recipes.favoriteAdd', $recipe->id) }}"
+         class="btn btn-sm btn-primary"
+         title="Add Favorite">
+         <i class="{{ Config::get('buttons.favorite') }} text-danger"></i>
+      </a>
+   @else
+      <a href="{{ route('recipes.favoriteRemove', $recipe->id) }}"
+         class="btn btn-sm btn-primary"
+         title="Remove Favorite">
+         <i class="{{ Config::get('buttons.favorite') }} text-success"></i>
+      </a>
+   @endif
+{{-- @endif --}}
