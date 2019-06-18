aaacca981af49a6462dd57ea44ea079c12252903
Stephane Leroux <stleroux@hotmail.ca>
2019-06-18 15:27:46 -0400

    Initial commit

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -12,10 +12,10 @@
 
    {!! Form::open(array('route' => 'users.store','method'=>'POST')) !!}
 
-      <div class="card mb-2">
+      <div class="card">
          <!--CARD HEADER-->
          <div class="card-header card_header">
-            <span class="h5 align-middle pt-2">
+            <span class="h5 align-middle">
                <i class="fas fa-user"></i>
                New User
             </span>
@@ -27,10 +27,67 @@
                @endif
             </span>
          </div>
+
          <!--CARD BODY-->
-         <div class="card-body card_body">
+         <div class="card-body card_body p-0">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+               <li class="nav-item">
+                  <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
+                     User Details
+                  </a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" id="core-tab" data-toggle="tab" href="#core" role="tab" aria-controls="core" aria-selected="true">
+                     Core Permissions
+                  </a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" id="nonCore-tab" data-toggle="tab" href="#nonCore" role="tab" aria-controls="nonCore" aria-selected="false">
+                     Non-Core Permissions
+                  </a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" id="modules-tab" data-toggle="tab" href="#modules" role="tab" aria-controls="modules" aria-selected="false">
+                     Modules Permissions
+                  </a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">
+                     Profile
+                  </a>
+               </li>
+            </ul>
+
+            <div class="tab-content pb-0 mb-0" id="myTabContent">
+               <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                  @include('users.inc.details')
+               </div>
+               <div class="tab-pane fade" id="core" role="tabpanel" aria-labelledby="core-tab">
+                  @include('users.inc.core')
+               </div>
+               <div class="tab-pane fade" id="nonCore" role="tabpanel" aria-labelledby="nonCore-tab">
+                  @include('users.inc.nonCore')
+               </div>
+               <div class="tab-pane fade" id="modules" role="tabpanel" aria-labelledby="modules-tab">
+                  @include('users.inc.modules')
+               </div>
+               <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                  @include('users.inc.profile')
+               </div>
+            </div>
+         </div>
+
+         <!-- CARD FOOTER -->
+         <div class="card-footer pt-1 pb-1 pl-2">
+            Fields marked with an <span class="required"></span> are required
+         </div>
+      </div>
+
+
+
+         
             {{-- @include('users.form', ['disabled'=>'']) --}}
-            <div class="row">
+{{--             <div class="row">
 
                <div class="col-sm-12 col-md-2">
                   <div class="form-group {{ $errors->has('username') ? 'has-error' : '' }}">
@@ -55,14 +112,12 @@
                   </div>
                </div>
 
-            </div>
-         </div>
-         <div class="card-footer pt-1 pb-1 pl-2">
-            Fields marked with an <span class="required"></span> are required
-         </div>
-      </div>
+            </div> --}}
+         
+         
+      {{-- </div> --}}
    
-      <div class="card mb-3">
+      {{-- <div class="card mb-3">
          <div class="card-header card_header">
             Select the permissions to assign to this user
          </div>
@@ -170,7 +225,7 @@
                </div>
             </div>
          </div>
-      </div>
+      </div> --}}
       
    {!! Form::close() !!}
 
