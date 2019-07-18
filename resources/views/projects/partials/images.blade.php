b23d7316c3246e5c9cbeb3a1a468929a8fd3bf81
Stephane <stleroux@hotmail.ca>
2019-07-18 16:55:23 -0400

    Initial commit

--- resources/views/projects/partials/images.blade.php
+++ resources/views/projects/partials/images.blade.php
@@ -107,7 +107,7 @@
             </button>
          </div>
          <div class="modal-body text-center">
-            <img src="" width="100%" height="450px" />
+            <img src="" />
             <p id="description"></p>
          </div>
          <div class="modal-footer p-1">
