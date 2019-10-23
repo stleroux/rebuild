5da1739b92df6cc79a6b401bfb0aa6ba264dcc14
Stephane Leroux <stleroux@hotmail.ca>
2019-10-23 15:30:09 -0400

    October 23 2019

--- resources/views/admin/articles/buttons/add.blade.php
+++ resources/views/admin/articles/buttons/add.blade.php
@@ -1,8 +1,12 @@
 @if(checkPerm('article_add'))
    <a href="{{ route('admin.articles.create') }}"
+      data-toggle="modal"
+      data-target="#add"
       class="btn {{ $size ? 'btn-'.$size : '' }} btn-success text-light"
       title="Add Article">
       <i class="{{ Config::get('buttons.add') }}"></i>
       {{ $btn_label ?? '' }}
    </a>
 @endif
+
+@include('admin.articles.modals.add')
---
+++ resources/views/admin/articles/modals/add.blade.php
@@ -0,0 +1,20 @@
+<!-- Modal Dialog -->
+<div class="modal fade" id="add" role="dialog" aria-labelledby="addLabel" aria-hidden="true">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="card">
+            <div class="modal-header card-header">
+               <h4 class="modal-title">Add Article</h4>
+               <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">
+               <p>Are you sure you want to add an article?</p>
+            </div>
+            <div class="modal-footer">
+               <button type="button" class="btn btn-success" id="confirm">YES</button>
+               <button type="button" class="btn btn-warning" data-dismiss="modal">NO</button>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
